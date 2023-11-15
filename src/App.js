da7561574dd32454e37d948a43911c5c4f9bed51
HasanCoder66 <hasanashraf874@gmail.com>
2023-11-16 00:38:35 +0500

    40:43

--- src/App.js
+++ src/App.js
@@ -1,164 +1,48 @@
-import './App.css';
-import styled , {ThemeProvider} from 'styled-components'
-import Menu from './components/Menu';
-import Navbar from './components/Navbar';
-import { darkTheme, lightTheme } from './components/utils/Theme';
-import { useState } from 'react';
+import './App.css'
+import styled, { ThemeProvider } from "styled-components";
+import Menu from "./components/Menu";
+import Navbar from "./components/Navbar";
+import { darkTheme, lightTheme } from "./components/utils/Theme";
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { Home } from '@mui/icons-material';
+import Home from "./pages/Home";
+import Video from "./pages/Video";
+
 
 const Container = styled.div`
-display : flex
-`
+  display: flex;
+`;
 const Main = styled.div`
-flex : 7;
-background-color : ${({theme }) => theme.bg};
-color : ${({theme }) => theme.text};
-`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text};
+`;
 const Wrapper = styled.div`
-`
+padding : 22px 96px
+`;
 function App() {
-  const [darkMode , setDarkMode ] = useState(true)
-    return (
+  const [darkMode, setDarkMode] = useState(true);
+  return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-    <Container>
-    <Menu darkMode={darkMode} setDarkMode={setDarkMode}/>
-    <Main>
-      <Navbar/>
-      <Wrapper>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-        <h1>
-        hasan ki videos ke cards
-        </h1>
-      </Wrapper>
-    </Main>
-    </Container>
+      <Container>
+        <BrowserRouter>
+          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Main>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path="/">
+                  <Route index element={<Home />} />
+                   <Route path="video">
+                    <Route path=":id" element={<Video />} />
+                  </Route>
+                </Route>
+              </Routes>
+            </Wrapper>
+          </Main>
+        </BrowserRouter>
+      </Container>
     </ThemeProvider>
   );
 }
